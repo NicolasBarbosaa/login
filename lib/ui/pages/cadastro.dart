9c28db7321b0bcd9b79b8636ba0b9d680238f973
unknown <senai@DSN1191137657.edu119.local>
2024-04-03 09:32:57 -0300

    TelaCadastro

--- lib/ui/pages/cadastro.dart
+++ lib/ui/pages/cadastro.dart
@@ -16,79 +16,86 @@ class Cadastro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  
-        appBar: AppBar(title: Text('Cadastro')),
-        body: Column(
-          children: [
-            
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                controller: nomeController,
-                decoration: const InputDecoration(
-                    hintText: 'Usuario', border: OutlineInputBorder()),
-              ),
-              
+      backgroundColor: Color.fromARGB(50, 252, 236, 221),
+      appBar: AppBar(title: Text('')),
+      body: Column(
+        children: [
+          Container(
+            width: 300,
+            height: 300,
+            child: Image.asset('assets/NikoStudie.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: TextFormField(
+              controller: nomeController,
+              decoration: const InputDecoration(
+                  hintText: 'Usuario', border: OutlineInputBorder()),
             ),
-            const SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                controller: senhaController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                    hintText: 'Senha', border: OutlineInputBorder()),
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: TextFormField(
+              controller: senhaController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                  hintText: 'Senha', border: OutlineInputBorder()),
             ),
-            const SizedBox(
-              height: 8,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (nomeController.text.isNotEmpty && senhaController.text.isNotEmpty) {
-
-                  if (UserDataBase.users.any((user) => user.name == nomeController.text)) {
-
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        backgroundColor: Colors.red,
-                        content: Text('Nome de usuário já está em uso'),
-                      ),
-                    );
-                  } else {
-                    UserModel userModel = UserModel(
-                        name: nomeController.text,
-                        password: senhaController.text);
-                    UserDataBase.addUser(userModel);
-
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => Login(
-                              newUser: UserModel(
-                                  name: nomeController.text,
-                                  password: senhaController.text))),
-                      (route) => false,
-                    );
-                    
-                    // Mostrar mensagem de sucesso
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        backgroundColor: Colors.green,
-                        content: Text('Cadastro realizado com sucesso'),
-                      ),
-                    );
-                  }
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (nomeController.text.isNotEmpty &&
+                  senhaController.text.isNotEmpty) {
+                if (UserDataBase.users
+                    .any((user) => user.name == nomeController.text)) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      backgroundColor: Colors.red,
+                      content: Text('Nome de usuário já está em uso'),
+                    ),
+                  );
                 } else {
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    backgroundColor: Colors.red,
-                    content: Text("Preencha todos os campos"),
-                  ));
+                  UserModel userModel = UserModel(
+                      name: nomeController.text,
+                      password: senhaController.text);
+                  UserDataBase.addUser(userModel);
+
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => Login(
+                            newUser: UserModel(
+                                name: nomeController.text,
+                                password: senhaController.text))),
+                    (route) => false,
+                  );
+
+                  // Mostrar mensagem de sucesso
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      backgroundColor: Colors.green,
+                      content: Text('Cadastro realizado com sucesso'),
+                    ),
+                  );
                 }
-              },
-              child: Text('Cadastrar'),
-            )
-          ],
-        ));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  backgroundColor: Colors.red,
+                  content: Text("Preencha todos os campos"),
+                ));
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromARGB(255, 255, 103, 1),
+              foregroundColor: Color.fromARGB(255, 43, 43, 43),
+            ),
+            child: Text('Cadastrar'),
+          )
+        ],
+      ),
+    );
   }
 }
